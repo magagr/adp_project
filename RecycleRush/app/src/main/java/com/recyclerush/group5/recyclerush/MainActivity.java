9378664d770ae34b6ce27e1870d0756f07b843d2
Jesper Olsson <jesper.olsson191@gmail.com>
2018-03-28 10:48:30 +0200

    Prompt user with the camera at start-up

--- RecycleRush/app/src/main/java/com/recyclerush/group5/recyclerush/MainActivity.java
+++ RecycleRush/app/src/main/java/com/recyclerush/group5/recyclerush/MainActivity.java
@@ -1,5 +1,7 @@
 package com.recyclerush.group5.recyclerush;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openCamera();
+    }
+
+    private void openCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
     }
 }
